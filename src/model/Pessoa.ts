63042cf1511fdebdc09346029f00f61e7b207f38
JoaoPedroTamiao <jpstz2013@hotmail.com>
2023-10-20 09:13:34 -0300

    Garantia dq esta versionado

--- src/model/Pessoa.ts
+++ src/model/Pessoa.ts
@@ -1,5 +1,5 @@
- export class Pessoa {
-    
+export class Pessoa {
+
     //atributos da classe
     private nome: string;
     private cpf: string;
@@ -9,23 +9,23 @@
     private altura: number;
     private peso: number;
 
-    
+
     //método construtor
-    public constructor (_nome: string, _cpf: string, _data_nascimento: Date, _telefone: string, _endereco: string, _altura: number, _peso: number){
-            this.nome = _nome;
-            this.cpf = _cpf;
-            this.data_nascimento = _data_nascimento;
-            this.telefone = _telefone;
-            this.endereco = _endereco;
-            this.altura = _altura;
-            this.peso = _peso;
+    public constructor(_nome: string, _cpf: string, _data_nascimento: Date, _telefone: string, _endereco: string, _altura: number, _peso: number) {
+        this.nome = _nome;
+        this.cpf = _cpf;
+        this.data_nascimento = _data_nascimento;
+        this.telefone = _telefone;
+        this.endereco = _endereco;
+        this.altura = _altura;
+        this.peso = _peso;
 
     }
 
 
     // métodos GETTERS and SETTERS
 
-    public getNome(){
+    public getNome() {
         return this.nome;
     }
 
@@ -34,7 +34,7 @@
      * @param _nome 
      * Nome da pessoa
      */
-    public setNome(_nome: string){
+    public setNome(_nome: string) {
         this.nome = _nome;
     }
 
@@ -43,7 +43,7 @@
      * Retorna o cpf da pessoa
      * @returns cpf: cpf da pessoa
      */
-    public getCPF(){
+    public getCPF() {
         return this.cpf;
     }
 
@@ -52,11 +52,11 @@
      * @param _cpf 
      * cpf da pessoa
      */
-    public setCPF(_cpf: string){
+    public setCPF(_cpf: string) {
         this.cpf = _cpf;
     }
 
-    public getData_Nascimento(){
+    public getData_Nascimento() {
         return this.data_nascimento;
     }
 
@@ -65,11 +65,11 @@
      * @param _data_nascimento 
      * Data de nascimento da pessoa
      */
-    public setData_Nascimento(_data_nascimento: Date){
+    public setData_Nascimento(_data_nascimento: Date) {
         this.data_nascimento = _data_nascimento;
     }
 
-    public getTelefone(){
+    public getTelefone() {
         return this.telefone;
     }
     /**
@@ -77,11 +77,11 @@
      * @param _telefone 
      * Telefone da pessoa
      */
-    public setTelefone(_telefone: string){
+    public setTelefone(_telefone: string) {
         this.telefone = _telefone;
     }
 
-    public getEndereco(){
+    public getEndereco() {
         return this.endereco;
     }
     /**
@@ -89,11 +89,11 @@
      * @param _endereco 
      * endereco da pessoa
      */
-    public setEndereco(_endereco: string){
+    public setEndereco(_endereco: string) {
         this.endereco = _endereco;
     }
 
-    public getAltura(){
+    public getAltura() {
         return this.altura;
     }
 
@@ -102,11 +102,11 @@
      * @param _altura 
      * altura da pessoa
      */
-    public setAltura(_altura: number){
+    public setAltura(_altura: number) {
         this.altura = _altura;
     }
 
-    public getPeso(){
+    public getPeso() {
         return this.peso;
     }
 
@@ -115,21 +115,21 @@
      * @param _peso 
      * peso da pessoa
      */
-    public setPeso(_peso: number){
+    public setPeso(_peso: number) {
         this.peso = _peso;
     }
 
     //implementar os métodos
-    public falar():void {
+    public falar(): void {
         console.log(`${this.nome} esta falando`);
     }
 
-    public falarFrase(_frase:string):void {
+    public falarFrase(_frase: string): void {
         //Logica de negócio
         console.log(`${this.nome} fala: ${_frase}`);
     }
 
-    public mostraPessoa(){
+    public mostraPessoa() {
         console.log(`Nome: ${this.nome}
                     CPF: ${this.cpf}
                     Data: ${this.data_nascimento}
@@ -139,28 +139,34 @@
                     Peso: ${this.peso}`);
     }
 
-    public andar():void {
+    public andar(): void {
         console.log(`${this.nome} esta andando`);
     }
 
-    public andandoDistancia(_Quilometros:number):void {
-        setTimeout(() =>{
+    public andandoDistancia(_Quilometros: number): void {
+        setTimeout(() => {
             console.log(`${this.nome} anda: ${_Quilometros} Quilometros`);
-        },3000);
-        
-        
+        }, 3000);
+
+
     }
 
 
-    public comer():void {
+    public comer(): void {
         console.log(`${this.nome} esta comendo`);
     }
 
-    public comendoComida(_prato:string):void {
+    public comendoComida(_prato: string): void {
         console.log(`${this.nome} esta comendo: Uma${_prato}`);
     }
 
 
+
+    /**
+     *  CRUD
+     */
+
+    
 
 
 
